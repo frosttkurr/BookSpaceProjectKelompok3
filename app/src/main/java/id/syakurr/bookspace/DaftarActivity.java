dd8434d5077bed1788d8a38858f53eb03acb389e
frosttkurr <thisiscyber13@gmail.com>
2021-11-12 15:33:29 +0800

    Commit pertama, GASKEUN AMPE KELAR BOS

---
+++ app/src/main/java/id/syakurr/bookspace/DaftarActivity.java
@@ -0,0 +1,117 @@
+package id.syakurr.bookspace;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DaftarActivity extends AppCompatActivity {
+    private Button daftar;
+    private EditText nik, nama, alamat, username, email, password;
+    private RadioGroup jenis_kelamin;
+    private RadioButton jk;
+    private CheckBox check_term;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar);
+
+        daftar = (Button)findViewById(R.id.btn_daftar);
+        nik = (EditText)findViewById(R.id.nik);
+        nama = (EditText)findViewById(R.id.nama);
+        alamat = (EditText)findViewById(R.id.alamat);
+        username = (EditText)findViewById(R.id.username);
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+        jenis_kelamin = (RadioGroup)findViewById(R.id.jenis_kelamin);
+        check_term = (CheckBox)findViewById(R.id.check_term);
+
+        if (!check_term.isChecked()){
+            daftar.setAlpha(.5f);
+            daftar.setClickable(false);
+        }
+
+        check_term.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!check_term.isChecked()){
+                    daftar.setAlpha(.5f);
+                    daftar.setClickable(false);
+                } else {
+                    daftar.setAlpha(1);
+                    daftar.setClickable(true);
+                }
+            }
+        });
+
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getEmail = email.getText().toString();
+                String checkEmail = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+                int selected_jk = jenis_kelamin.getCheckedRadioButtonId();
+                jk = (RadioButton) findViewById(selected_jk);
+
+                if (nik.length() == 0 && nama.length() == 0 && alamat.length() == 0 && jenis_kelamin.getCheckedRadioButtonId() == -1
+                        && username.length() == 0 && email.length() == 0 && password.length() == 0) {
+                    Toast.makeText(DaftarActivity.this, "Lengkapi form pendaftaran!", Toast.LENGTH_SHORT).show();
+                } else if (nik.length() < 16 || nik.length() > 16) {
+                    Toast.makeText(DaftarActivity.this, "NIK harus 16 digit!", Toast.LENGTH_SHORT).show();
+                } else if (nama.length() == 0) {
+                    Toast.makeText(DaftarActivity.this, "Nama tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                } else if (alamat.length() == 0) {
+                    Toast.makeText(DaftarActivity.this, "Alamat tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                } else if (jenis_kelamin.getCheckedRadioButtonId() == -1) {
+                    Toast.makeText(DaftarActivity.this, "Jenis kelamin tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                } else if (username.length() == 0) {
+                    Toast.makeText(DaftarActivity.this, "Username tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                } else if (email.length() == 0) {
+                    Toast.makeText(DaftarActivity.this, "Email tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                } else if (!getEmail.matches(checkEmail)) {
+                    Toast.makeText(DaftarActivity.this, "Email tidak valid!", Toast.LENGTH_SHORT).show();
+                } else if (password.length() == 0) {
+                    Toast.makeText(DaftarActivity.this, "Password tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                } else if (password.length() < 8) {
+                    Toast.makeText(DaftarActivity.this, "Password minimal 8 karakter!", Toast.LENGTH_SHORT).show();
+                }
+                dialogAlert();
+            }
+        });
+    }
+
+    private void dialogAlert(){
+        AlertDialog.Builder dialogAlertBuilder = new AlertDialog.Builder(DaftarActivity.this);
+        dialogAlertBuilder.setTitle("Konfirmasi Data");
+        dialogAlertBuilder
+                .setMessage("NIK : " +nik.getText().toString()+ "\n" +
+                            "Nama : " +nama.getText().toString()+ "\n" +
+                            "Alamat : " +alamat.getText().toString()+ "\n" +
+                            "Jenis Kelamin : " +jk.getText().toString()+ "\n" +
+                            "Email : " +email.getText().toString()+ "\n" +
+                            "Username : " +username.getText().toString()+ "\n")
+                .setPositiveButton("Konfirmasi", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(DaftarActivity.this, "Pendaftaran Berhasil", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog dialog = dialogAlertBuilder.create();
+
+        dialog.show();
+    }
+}
---
+++ app/src/main/java/id/syakurr/bookspace/MainActivity.java
@@ -0,0 +1,46 @@
+package id.syakurr.bookspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button login;
+    private EditText username, password;
+    private TextView daftar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = (Button)findViewById(R.id.btn_login);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        daftar = (TextView)findViewById(R.id.link_daftar);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.length() == 0 || password.length() == 0) {
+                    Toast.makeText(MainActivity.this, "Lengkapi Username atau Password!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoRegist = new Intent(MainActivity.this, DaftarActivity.class);
+                startActivity(gotoRegist);
+            }
+        });
+    }
+}
